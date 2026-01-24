34c423bccfd339a029f8825706aa9f748bdf1ef7
Codder-Zee <gsonune8010@gmail.com>
2026-01-24 08:31:01 +0530

    Delete generate_subtitles.py

--- generate_subtitles.py
+++
@@ -1,9 +0,0 @@
-import textwrap
-
-text = open("script.txt", encoding="utf-8").read()
-
-lines = textwrap.wrap(text, width=25)
-
-with open("subs.txt", "w", encoding="utf-8") as f:
-    for line in lines[:6]:  # Shorts ke liye enough
-        f.write(line + "\n")
